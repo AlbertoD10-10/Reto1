160d5299f7465157b8d3a2ecc35d4f5b38f29ecf
piperuao26 <piperuao26@gmail.com>
2024-08-26 23:06:00 -0500

    chord inicial, hay que probar los comandos del proto, pero en principio, asi se haria

--- client.py
+++ client.py
@@ -1,15 +1,18 @@
 import grpc
-import services.fileservice_pb2_grpc as pb2_grpc
-import services.fileservice_pb2 as pb2
-
+import services.fileservice_pb2_grpc 
+import services.fileservice_pb2
 def run():
-    with grpc.insecure_channel('127.0.0.1:50051') as channel:
-        stub = pb2_grpc.FileServiceStub(channel)
-        response = stub.UploadFile(pb2.FileRequest(filename="example.txt"))
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = services.fileservice_pb2_grpc .FileServiceStub(channel)
+        response = stub.UploadFile(services.fileservice_pb2.FileRequest(filename="example.txt"))
         print("Upload response: ", response.message)
 
-        response = stub.DownloadFile(pb2.FileRequest(filename="example.txt"))
+        response = stub.DownloadFile(services.fileservice_pb2.FileRequest(filename="example.txt"))
         print("Download response: ", response.message)
+
+        id_to_find = 5
+        response = stub.FindSuccessor(services.fileservice_pb2.NodeID(id = id_to_find))
+        print(f"El sucesor del ID {id_to_find} es el nodo con ID {response.id} en {response.ip}:{response.port}")
 
 if __name__ == "__main__":
     run()
